0db51b7f77b6b4fb1f36ef97008496e5476a80c4
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-11-20 08:37:33 +0300

    the logic of updating and deleting cells has been implemented

---
+++ TaskList/CoreData/StorageManager.swift
@@ -0,0 +1,16 @@
+//
+//  StorageManager.swift
+//  TaskList
+//
+//  Created by Roman on 18.11.2022.
+//
+
+import CoreData
+import UIKit
+
+class StorageManager {
+//    private let viewContext = {
+//        guard let context = UIApplication.shared.delegate as? AppDelegate else { return }
+//        return context.persistentContainer.viewContext
+//    }()
+}
---
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,154 @@
+//
+//  TaskListViewController.swift
+//  TaskList
+//
+//  Created by Roman on 17.11.2022.
+//
+
+import UIKit
+import CoreData
+
+
+
+class TaskListViewController: UITableViewController {
+    
+    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let cellID = "task"
+    private var taskList: [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        fetchData()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = UIColor(named: "MilkBlue")
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        showAlert(withTitle: "New Task", withMessage: "What do you want to do?", andText: "")
+    }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            taskList = try viewContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func showAlert(withTitle title: String, withMessage message: String, andText text: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            if text == "" {
+                save(task)
+            } else {
+                update(task)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "Enter the task"
+            textField.text = text
+        }
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        let task = Task(context: viewContext)
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+    
+    private func update(_ taskName: String) {
+        let task = Task(context: viewContext)
+        task.title = taskName
+        
+        let cellIndex = IndexPath(row: tableView.indexPathForSelectedRow?.row ?? 0, section: 0)
+        taskList[cellIndex.row].title = taskName
+        tableView.reloadData()
+        
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+}
+
+// MARK: - UITableView Data Source
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        showAlert(
+            withTitle: "Update Task",
+            withMessage: "Do you want to change the task?",
+            andText: taskList[indexPath.row].title ?? ""
+        )
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            self.taskList.remove(at: indexPath.row)
+            
+            self.tableView.reloadData()
+        }
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        configuration.performsFirstActionWithFullSwipe = true
+        return configuration
+    }
+}
